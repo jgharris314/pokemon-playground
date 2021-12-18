d01d437dbb8142aafd43b4fdcc9930a54f1d7938
Jacob Harris <jgharris314@gmail.com>
2021-12-18 00:00:42 -0700

    prettied things up

--- src/App.tsx
+++ src/App.tsx
@@ -44,15 +44,15 @@ function App() {
 	}
 
 	return (
-		<div className="App">
-			<h1 className="text-3xl font-bold underline">Pokemon</h1>
-			<div className="box-border h-52 w-32 bg-slate-200 rounded-xl  shadow-lg overflow-hidden m-5 p-2 hover:overflow-scroll md:max-w-2xl poke-list">
+		<div className="App flex flex-col">
+			<h1 className="text-3xl font-bold underline">Pokemon Playground</h1>
+			<div className="box-border h-60  w-48 overflow-y-scroll bg-slate-200 rounded-xl  shadow-lg m-5 p-2 md:max-w-2xl poke-list">
 				{pokemonGroup
 					? pokemonGroup.results.map((p: Pokemon, index: number) => (
 							<li
 								onClick={() => loadIndividualPokemon(p.url)}
 								key={index}
-								className="pokemon-group hover:bg-slate-600 hover:text-white"
+								className="pokemon-group hover:bg-slate-600 hover:text-white hover:rounded-xl"
 							>
 								{p.name.charAt(0).toUpperCase() +
 									p.name.slice(1)}
@@ -60,47 +60,58 @@ function App() {
 					  ))
 					: null}
 			</div>
-			<button
-				onClick={() =>
-					nextPokemonGroup(pokemonGroup ? pokemonGroup.next : "")
-				}
-			>
-				Next Set
-			</button>
-			<button
-				onClick={() =>
-					nextPokemonGroup(pokemonGroup ? pokemonGroup.previous : "")
-				}
-			>
-				Previous Set
-			</button>
+			<div className="flex flex-row">
+				<button
+					onClick={() =>
+						nextPokemonGroup(
+							pokemonGroup ? pokemonGroup.previous : ""
+						)
+					}
+					className="bg-yellow-500 w-32 m-5 rounded-xl p-1 hover:bg-yellow-300"
+				>
+					Previous Set
+				</button>
+				<button
+					onClick={() =>
+						nextPokemonGroup(pokemonGroup ? pokemonGroup.next : "")
+					}
+					className="bg-green-500 w-32 m-5 rounded-xl p-1 hover:bg-green-300"
+				>
+					Next Set
+				</button>
+			</div>
 			{individualPokemon && (
-				<div>
-					<div>
+				<div className="flex flex-col w-1/4 rounded-xl justify-center items-center bg-slate-200 shadow-lg p-1">
+					<div className="flex flex-row">
 						{individualPokemon.name.charAt(0).toUpperCase() +
 							individualPokemon.name.slice(1)}
 					</div>
-					<div>{individualPokemon.id}</div>
-					<img
-						src={individualPokemon.sprites.front_default}
-						alt={`${
-							individualPokemon.name.charAt(0).toUpperCase() +
-							individualPokemon.name.slice(1)
-						} facing frontwards`}
-					/>
-					<div>
+					<div className="flex flex-row w-full justify-center items-center">
+						ID: {individualPokemon.id}
+					</div>
+					<div className="flex flex-row w-full justify-center items-center">
+						<img
+							src={individualPokemon.sprites.front_default}
+							alt={`${
+								individualPokemon.name.charAt(0).toUpperCase() +
+								individualPokemon.name.slice(1)
+							} facing frontwards`}
+							className="w-1/2 m-auto"
+						/>
+					</div>
+					<div className="flex flex-row w-full items-center justify-center">
 						{individualPokemon.types.map(
 							(type: PokemonTypes, index: number) => {
 								return (
-									<li
-										className={`type ${type.type.name}`}
+									<div
+										className={`flex flex-col w-1/4 rounded-xl mx-1 p-0 ${type.type.name}`}
 										key={index}
 									>
 										{type.type.name
 											.charAt(0)
 											.toUpperCase() +
 											type.type.name.slice(1)}
-									</li>
+									</div>
 								);
 							}
 						)}
